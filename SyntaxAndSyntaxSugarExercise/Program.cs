9257a3e8606c48af5fde9d5651ad0200735bd1c5
Haroutun Panosyan <harupanosyan@gmail.com>
2023-06-22 10:07:29 -0700

    changed to inferred type and the if else into a ternary.

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Changed answer and response into inferred type.
+            var answer = 4;
+            var response = "";
+            // Changed if else statement into its' ternary equivalent.
+            response = answer < 9 ? 
+                $"{answer} is less than nine." : 
+                $"{answer} is greater than or equal to nine";
+            // Print out response.
+            Console.WriteLine(response);
         }
     }
 }
